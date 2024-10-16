f22b2b560f892e425ae66d95200e12f2c987960e
Vishvaraj18 <vishvaraj.dgkgr22@sinhgad.edu>
2024-10-17 00:20:21 +0530

    updated

--- index.js
+++ index.js
@@ -44,61 +44,32 @@ const connectDB = async () => {
 
 
 app.get('/',async (req,res)=>{
-  const tasks = await collection.find({});
-  // fs.readdir(`files`,(err,files)=>{   
+  const tasks = await collection.find({}); 
   res.render('index.ejs',{tasks}); 
   });
-// });
+
 
 app.post('/create',async (req,res)=>{
-  // fs.writeFile(`files/${req.body.title.split(' ').join('')}.txt`,req.body.content,(err)=>{ 
-  // })
+ 
   const {title ,content} = req.body;
    
 
   const newTask = new collection({ taskName: title, taskContent:content });
   await newTask.save();
   res.redirect('/')
-  // const name = req.body.title;
-  // const desc = req.body.content;
-  // const tutSchema = new mongoose.Schema({
-  //   taskName:{
-  //     type:String,
-  //     required:true
-  //   },
-  //   taskContent:{
-  //     type:String,
-  //     required:true
-  //   }
-   
-  // })
-  //created a collection by
-  //add data in existing collection or create a new collection 
-  // const collection =mongoose.models.tasks || new mongoose.model('tasks',tutSchema)
-  
-  // data={
-  //  taskName:name,
-  //    taskContent:desc,
-     
-  //  }
-  // collection.insertMany([data])
+ 
 });
 
-app.get('/readMore/:filename',(req,res)=>{
-  fs.readFile(`files/${req.params.filename}`,'utf-8',function(err,filedata){
-    res.render('readmore',{filename: req.params.filename,filedata:filedata});
-  })
-})
+
 
 app.get('/edit/:filename',async (req,res)=>{
-  // res.render('edit',{previous:req.params.filename,previousContent:req.body.content})
+
   const task = await collection.findById(req.params.filename);
   res.render('edit.ejs', { task });
 })
 
 app.post('/edit/:filename',async (req,res)=>{
-  // fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,(err)=>{
-    //  console.log(req.body);
+
     try{
       const {title ,content } = req.body;
     await collection.findByIdAndUpdate(req.params.filename,{taskName:title,taskContent:content
@@ -109,7 +80,7 @@ app.post('/edit/:filename',async (req,res)=>{
     res.redirect('/')
     
   }
-  // })
+
 })
 app.post('/delete/:filename',async (req,res)=>{
   await collection.findByIdAndDelete(req.params.filename);
